b7417ebbc71d42359b36bfdd4b9b181d9f324fa8
Mallet Flora <floramallet@outlook.fr>
2021-01-18 14:18:23 +0100

    Line animation in the Home page and clean responsive

--- portfolio/src/components/Ball.vue
+++ portfolio/src/components/Ball.vue
@@ -1,15 +1,15 @@
 <template lang="html">
   <div class="ball">
-    <svg width="500" height="350">
-      <circle id="blue-circle" r="7" cx="50" cy="50" fill="#1b1464" />
+    <svg width="20" height="100">
+      <circle id="blue-circle" r="7" cx="10" cy="10" fill="#1b1464" />
       <animate 
         xlink:href="#blue-circle"
         attributeName="cy"
-        from="50" to="100" 
+        from="10" to="60" 
         dur="3s"
         begin="click"
         calcMode="spline"
-        values="50; 100; 70; 100; 85; 100; 50"
+        values="10; 60; 30; 60; 45; 60; 10"
         keyTimes="0; 0.15; 0.3; 0.45; 0.6; 0.75; 1"
         keySplines="0.42 0 1 1;
                     0 0 0.59 1;
